a78a483b96bc3269840d6d0ed2d9dfaa2a89ccb3
minhcuong99er <senseicuong@gmail.com>
2021-05-01 22:56:23 +0700

    Update

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -18,20 +18,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clickableViews: List<View> =
-            listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text, red_button, yellow_button, green_button)
+                listOf(box_one_text, box_two_text, box_three_text,
+                        box_four_text, box_five_text, constraint_layout,
+                        red_button, green_button, yellow_button)
 
         for (item in clickableViews) {
-            item.setOnClickListener { makeColored(item) }
+            item.setOnClickListener { makeColored(it) }
         }
     }
     
     @SuppressLint("ResourceAsColor")
     private fun makeColored(view: View) {
         when (view.id) {
-
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
